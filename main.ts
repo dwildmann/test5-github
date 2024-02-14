55d6d219117f92155aeb7cb517edc1b55f2636cf
dwildmann <d.wildmann@gmail.com>
2024-02-14 13:48:57 +0100

    dauerhaft

--- main.ts
+++ main.ts
@@ -7,6 +7,12 @@ basic.showLeds(`
     . . # . .
     . . . . .
     `)
+basic.pause(100)
 basic.forever(function () {
-	
+    for (let Index = 0; Index <= 4; Index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+        basic.showString("" + (Index))
+        basic.pause(100)
+    }
 })
