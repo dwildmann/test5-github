ef5263c608b42f038a6522cf24081259f3741411
dwildmann <d.wildmann@gmail.com>
2024-02-15 15:26:00 +0100

    lamp implemented

--- main.py
+++ main.py
@@ -8,6 +8,34 @@ def on_button_pressed_a():
     strip.show()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def wechsel_regenbogen():
+    while True:
+        if input.button_is_pressed(Button.B):
+            return
+        for Index in range(256):
+            strip.show_rainbow(1, 360)
+            strip.set_brightness(Index)
+            strip.show()
+            basic.pause(20)
+        for Index2 in range(256):
+            strip.show_rainbow(1, 360)
+            strip.set_brightness(255 - Index2)
+            strip.show()
+            basic.pause(20)
+
+def on_button_pressed_ab():
+    global Eingeschaltet
+    if Eingeschaltet == True:
+        strip.set_brightness(0)
+        strip.show_color(Farbe)
+        strip.show()
+    else:
+        strip.set_brightness(Helligkeit)
+        strip.show_color(Farbe)
+        strip.show()
+    Eingeschaltet = not (Eingeschaltet)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
 def on_button_pressed_b():
     global FarbZaehler, Farbe
     FarbZaehler += 1
@@ -19,34 +47,69 @@ def on_button_pressed_b():
     elif FarbZaehler == len(FarbListe):
         strip.show_rainbow(1, 360)
     elif FarbZaehler == len(FarbListe) + 1:
-        Farbe = neopixel.rgb(120, 50, 30)
-        strip.set_brightness(Helligkeit)
-        strip.show_color(Farbe)
-        strip.show()
+        wechsel_regenbogen()
     else:
-        Farbe = neopixel.rgb(30, 50, 120)
-        strip.set_brightness(Helligkeit)
-        strip.show_color(Farbe)
-        strip.show()
+        wechsel_farbig()
         FarbZaehler = 0
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def wechsel_farbig():
+    global st_rot, st_gruen, st_blau, st_delta_rot, st_delta_gruen, st_delta_blau, Farbe
+    st_rot = 0
+    st_gruen = 127
+    st_blau = 255
+    st_delta_rot = 2
+    st_delta_gruen = 2
+    st_delta_blau = 2
+    while True:
+        if input.button_is_pressed(Button.B):
+            return
+        Farbe = neopixel.rgb(st_rot, st_gruen, st_blau)
+        strip.show_color(Farbe)
+        strip.set_brightness(Helligkeit)
+        strip.show()
+        basic.pause(50)
+        st_rot += st_delta_rot
+        st_gruen += st_delta_gruen
+        st_blau += st_delta_blau
+        if st_rot > 255:
+            st_rot = 255
+            st_delta_rot = st_delta_rot * -1
+        if st_rot < 0:
+            st_rot = 0
+            st_delta_rot = st_delta_rot * -1
+        if st_gruen > 255:
+            st_gruen = 255
+            st_delta_gruen = st_delta_gruen * -1
+        if st_gruen < 0:
+            st_gruen = 0
+            st_delta_gruen = st_delta_gruen * -1
+        if st_blau > 255:
+            st_blau = 255
+            st_delta_blau = st_delta_blau * -1
+        if st_blau < 0:
+            st_blau = 0
+            st_delta_blau = st_delta_blau * -1
+st_delta_blau = 0
+st_delta_gruen = 0
+st_delta_rot = 0
+st_blau = 0
+st_gruen = 0
+st_rot = 0
 FarbListe: List[number] = []
+Eingeschaltet = False
 Helligkeit = 0
 Farbe = 0
+FarbZaehler = 0
 strip: neopixel.Strip = None
 FarbZaehler = 0
-FarbZaehler = 0
 strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
-Farbe = neopixel.rgb(255, 255, 255)
+Farbe = neopixel.rgb(255, 200, 25)
 strip.show_color(Farbe)
 Helligkeit = 102
+Eingeschaltet = True
 strip.show()
 FarbListe = [neopixel.rgb(255, 0, 0),
     neopixel.rgb(0, 255, 0),
     neopixel.rgb(0, 0, 255),
-    neopixel.rgb(255, 255, 255)]
-
-def on_forever():
-    pass
-basic.forever(on_forever)
+    neopixel.rgb(255, 200, 25)]
--- main.ts
+++ main.ts
@@ -7,6 +7,41 @@ input.onButtonPressed(Button.A, function () {
     strip.showColor(Farbe)
     strip.show()
 })
+function wechsel_regenbogen () {
+    while (true) {
+        for (let Index = 0; Index <= 205; Index++) {
+            if (input.buttonIsPressed(Button.B)) {
+                return
+            }
+            strip.showRainbow(1, 360)
+            strip.setBrightness(50 + Index)
+            strip.show()
+            basic.pause(10)
+        }
+        basic.pause(100)
+        for (let Index2 = 0; Index2 <= 205; Index2++) {
+            if (input.buttonIsPressed(Button.B)) {
+                return
+            }
+            strip.showRainbow(1, 360)
+            strip.setBrightness(255 - Index2)
+            strip.show()
+            basic.pause(10)
+        }
+    }
+}
+input.onButtonPressed(Button.AB, function () {
+    if (Eingeschaltet == true) {
+        strip.setBrightness(0)
+        strip.showColor(Farbe)
+        strip.show()
+    } else {
+        strip.setBrightness(Helligkeit)
+        strip.showColor(Farbe)
+        strip.show()
+    }
+    Eingeschaltet = !(Eingeschaltet)
+})
 input.onButtonPressed(Button.B, function () {
     FarbZaehler += 1
     if (FarbZaehler < FarbListe.length) {
@@ -17,35 +52,79 @@ input.onButtonPressed(Button.B, function () {
     } else if (FarbZaehler == FarbListe.length) {
         strip.showRainbow(1, 360)
     } else if (FarbZaehler == FarbListe.length + 1) {
-        Farbe = neopixel.rgb(120, 50, 30)
-        strip.setBrightness(Helligkeit)
-        strip.showColor(Farbe)
-        strip.show()
+        wechsel_regenbogen()
     } else {
-        Farbe = neopixel.rgb(30, 50, 120)
-        strip.setBrightness(Helligkeit)
-        strip.showColor(Farbe)
-        strip.show()
+        wechsel_farbig()
         FarbZaehler = 0
     }
 })
+function wechsel_farbig () {
+    st_rot = 0
+    st_gruen = 127
+    st_blau = 255
+    st_delta_rot = 2
+    st_delta_gruen = 2
+    st_delta_blau = 2
+    while (true) {
+        if (input.buttonIsPressed(Button.B)) {
+            return
+        }
+        Farbe = neopixel.rgb(st_rot, st_gruen, st_blau)
+        strip.showColor(Farbe)
+        strip.setBrightness(Helligkeit)
+        strip.show()
+        basic.pause(50)
+        st_rot += st_delta_rot
+        st_gruen += st_delta_gruen
+        st_blau += st_delta_blau
+        if (st_rot > 255) {
+            st_rot = 255
+            st_delta_rot = st_delta_rot * -1
+        }
+        if (st_rot < 0) {
+            st_rot = 0
+            st_delta_rot = st_delta_rot * -1
+        }
+        if (st_gruen > 255) {
+            st_gruen = 255
+            st_delta_gruen = st_delta_gruen * -1
+        }
+        if (st_gruen < 0) {
+            st_gruen = 0
+            st_delta_gruen = st_delta_gruen * -1
+        }
+        if (st_blau > 255) {
+            st_blau = 255
+            st_delta_blau = st_delta_blau * -1
+        }
+        if (st_blau < 0) {
+            st_blau = 0
+            st_delta_blau = st_delta_blau * -1
+        }
+    }
+}
+let st_delta_blau = 0
+let st_delta_gruen = 0
+let st_delta_rot = 0
+let st_blau = 0
+let st_gruen = 0
+let st_rot = 0
 let FarbListe: number[] = []
+let Eingeschaltet = false
 let Helligkeit = 0
 let Farbe = 0
 let strip: neopixel.Strip = null
 let FarbZaehler = 0
 FarbZaehler = 0
 strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
-Farbe = neopixel.rgb(255, 255, 255)
+Farbe = neopixel.rgb(255, 200, 25)
 strip.showColor(Farbe)
 Helligkeit = 102
+Eingeschaltet = true
 strip.show()
 FarbListe = [
 neopixel.rgb(255, 0, 0),
 neopixel.rgb(0, 255, 0),
 neopixel.rgb(0, 0, 255),
-neopixel.rgb(255, 255, 255)
+neopixel.rgb(255, 200, 25)
 ]
-basic.forever(function () {
-	
-})
